3bc3206c20c513768f97f8dbb89d53f439f18a2d
Kurahara-Dakota-5045 <Dakota.Kurahara.5045@mail.linnbenton.edu>
2019-10-22 13:08:09 -0700

    Assignment-1 projects.

---
+++ data_types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+int number_1 = 64;
+char car_1 = 'H';
+float number_2 = 10.55;
+char str[20] = "HelloWorld";
+printf("%s\n", str);
+printf("a float number is %f\n", number_2);
+printf("%c ello World\n", car_1);
+printf("8 squared is %d\n" , number_1);
+}
+
+
